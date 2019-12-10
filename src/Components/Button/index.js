2fd015b6c1979d659ffb5e783700d25189241656
John Hasznosi <noctiscreo@gmail.com>
2019-12-10 14:57:03 +0000

    Added button to App.js.

--- src/Components/Button/index.js
+++ src/Components/Button/index.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 
 export class Button extends Component {
     render () {
+
         return  (
             <button onClick={this.props.action}>{this.props.value}</button>
         )
